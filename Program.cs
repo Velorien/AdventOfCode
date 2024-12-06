5c80db406cc50a8a9bf2dc564a86d070156b8122
Velorien <k.miczkowski@outlook.com>
2024-12-06 19:00:43 +0100

    Added timer

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using AdventOfCode;
 
 Run(2024, 6, 2);
@@ -17,7 +18,10 @@ void Run(int year, int day, int task)
 
     var dayObject = Activator.CreateInstance(dayType) as IDay;
 
+    var sw = Stopwatch.StartNew();
     var data = File.ReadAllLines($"Aoc{year}/data/day{day}.txt");
     if (task == 1) dayObject!.Run1(data);
     else dayObject!.Run2(data);
+    sw.Stop();
+    Console.WriteLine($"Took {sw.ElapsedMilliseconds} milliseconds");
 }
